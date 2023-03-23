ad4c397bfc6b105e58992be95dd05f9fecfad625
brenogarcia1993 <brenogarciamail@gmail.com>
2023-03-23 14:16:31 -0300

    22.03

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ pauseButton.addEventListener('click', buttonPause)  //Callback
 stopButton.addEventListener('click', buttonStop)    //Callback
 soundOnButton.addEventListener('click', buttonSound)
 soundOffButton.addEventListener('click', buttonSound)
-setButton.addEventListener('click', buttonSetMinutes)
+setButton.addEventListener('click', buttonSetTimer)
 
 function buttonPlay() {
     playButton.classList.add('hide')
@@ -31,16 +31,43 @@ function buttonPlay() {
 
 function countdown() {
     setTimeout(function() {
-        let seconds = Number(secondsDisplay.innerHTML)
+        let seconds = Number(secondsDisplay.textContent)
+        let minutes = Number(minutesDisplay.textContent)
 
         if(seconds <= 0) {
             seconds = 60
-        }
 
-        secondsDisplay.innerHTML = seconds - 1
+            minutesDisplay.textContent = String(minutes - 1).padStart(2, "0")
+        }
+        
+        secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+
+        if(minutes <= 0 && seconds == 0) {
+            
+            setButton.classList.remove('hide')
+            stopButton.classList.add('hide')
+            pauseButton.classList.add('hide')
+            playButton.classList.remove('hide')
+            
+            return
+
+        }
         countdown()
+        
     }, 1000)
 }
+
+// function leftZero() {
+//     let secondsWithZero = Number(secondsDisplay.innerHTML)
+
+//     if(seconds < 10) {
+//         secondsWithZero = '0' + secondsWithZero
+//     }
+        
+//     secondsDisplay.innerHTML = secondsWithZero
+//     leftZero()
+// }
+
 
 function buttonPause() {
     pauseButton.classList.add('hide')
@@ -53,6 +80,7 @@ function buttonStop() {
     pauseButton.classList.add('hide')
     playButton.classList.remove('hide')
     
+    
 }
 
 function buttonSound() {
@@ -60,9 +88,9 @@ function buttonSound() {
     soundOffButton.classList.toggle('hide')
 }
 
-function buttonSetMinutes() {
+function buttonSetTimer() {
     minutes = prompt('Quantos minutos?')
-    minutesDisplay.innerHTML = minutes
+    minutesDisplay.innerHTML = String(minutes).padStart(2, "0")
     seconds = prompt('Quantos segundos?')
     secondsDisplay.innerHTML = seconds
     
