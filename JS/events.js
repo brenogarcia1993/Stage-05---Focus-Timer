741484e70bd6ae332018f8e0ff3c828626d69d91
brenogarcia1993 <brenogarciamail@gmail.com>
2023-04-19 20:17:28 -0300

    19.04 - 20:17

---
+++ JS/events.js
@@ -0,0 +1,55 @@
+export default function Events({
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSoundOn,
+    buttonSoundOff,
+    buttonSetTimer, 
+})
+
+playButton.addEventListener("click", buttonPlay); //Callback
+pauseButton.addEventListener("click", buttonPause); //Callback
+stopButton.addEventListener("click", buttonStop); //Callback
+soundOnButton.addEventListener("click", buttonSoundOn);
+soundOffButton.addEventListener("click", buttonSoundOff);
+setButton.addEventListener("click", buttonSetTimer);
+
+function buttonPlay() {
+  controls.play();
+  timer.countdown();
+  sound.pressButton();
+}
+
+function buttonPause() {
+  controls.pause();
+  timer.hold();
+  sound.pressButton();
+}
+
+function buttonStop() {
+  controls.reset();
+  timer.reset();
+  sound.pressButton();
+}
+
+function buttonSoundOn() {
+  soundOnButton.classList.add("hide");
+  soundOffButton.classList.remove("hide");
+  sound.bgAudio.play();
+}
+
+function buttonSoundOff() {
+  soundOnButton.classList.remove("hide");
+  soundOffButton.classList.add("hide");
+  sound.bgAudio.pause();
+}
+function buttonSetTimer() {
+  let newMinutes = controls.getMinutes();
+  if (!newMinutes) {
+    timer.reset();
+    return;
+  }
+
+  timer.updateDisplay(newMinutes, 0);
+  timer.updateMinutes(newMinutes);
+}
--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,8 @@ import Controls from "./controls.js"
 // named import
 import Timer from "./timer.js"
 import Sound from "./sounds.js"
+import Events from "./events.js"
+
 import { 
     playButton,
     pauseButton,
@@ -36,58 +38,58 @@ const timer = Timer({
 })
 
 
-playButton.addEventListener('click', buttonPlay)    //Callback
-pauseButton.addEventListener('click', buttonPause)  //Callback
-stopButton.addEventListener('click', buttonStop)    //Callback
-soundOnButton.addEventListener('click', buttonSoundOn)
-soundOffButton.addEventListener('click', buttonSoundOff)
-setButton.addEventListener('click', buttonSetTimer)
+// playButton.addEventListener('click', buttonPlay)    //Callback
+// pauseButton.addEventListener('click', buttonPause)  //Callback
+// stopButton.addEventListener('click', buttonStop)    //Callback
+// soundOnButton.addEventListener('click', buttonSoundOn)
+// soundOffButton.addEventListener('click', buttonSoundOff)
+// setButton.addEventListener('click', buttonSetTimer)
 
-function buttonPlay() {
+// function buttonPlay() {
     
-    controls.play()
-    timer.countdown()
-    sound.pressButton()
-}
+//     controls.play()
+//     timer.countdown()
+//     sound.pressButton()
+// }
 
 
-function buttonPause() {
-    controls.pause()
-    timer.hold()
-    sound.pressButton()
+// function buttonPause() {
+//     controls.pause()
+//     timer.hold()
+//     sound.pressButton()
     
-}
+// }
 
-function buttonStop() {
-    controls.reset()
-    timer.reset()
-    sound.pressButton()
+// function buttonStop() {
+//     controls.reset()
+//     timer.reset()
+//     sound.pressButton()
     
     
-}
+// }
 
-function buttonSoundOn() {
-    soundOnButton.classList.add('hide')
-    soundOffButton.classList.remove('hide')
-    sound.bgAudio.play()
-}
+// function buttonSoundOn() {
+//     soundOnButton.classList.add('hide')
+//     soundOffButton.classList.remove('hide')
+//     sound.bgAudio.play()
+// }
 
-function buttonSoundOff() {
-    soundOnButton.classList.remove('hide')
-    soundOffButton.classList.add('hide')
-    sound.bgAudio.pause()
-}
-function buttonSetTimer() {
-    let newMinutes = controls.getMinutes()
-        if (!newMinutes) {
-           timer.reset()
-           return
-        }
+// function buttonSoundOff() {
+//     soundOnButton.classList.remove('hide')
+//     soundOffButton.classList.add('hide')
+//     sound.bgAudio.pause()
+// }
+// function buttonSetTimer() {
+//     let newMinutes = controls.getMinutes()
+//         if (!newMinutes) {
+//            timer.reset()
+//            return
+//         }
 
         
-        timer.updateDisplay(newMinutes, 0)
-        timer.updateMinutes(newMinutes)
+//         timer.updateDisplay(newMinutes, 0)
+//         timer.updateMinutes(newMinutes)
        
        
-}
+// }
 
